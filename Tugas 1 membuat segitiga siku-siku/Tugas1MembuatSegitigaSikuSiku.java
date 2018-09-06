536814cae27d231a1501747074c53f33bc64dfba
Habib-Nur <42966402+Habib-Nur@users.noreply.github.com>
2018-09-06 21:59:20 +0700

    Add files via upload

---
+++ Tugas 1 membuat segitiga siku-siku/Tugas1MembuatSegitigaSikuSiku.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pkg1.membuat.segitiga.siku.siku;
+
+/**
+ *
+ * @author HABIB NUR ROHMAN
+ */
+public class Tugas1MembuatSegitigaSikuSiku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for(int a=0;a<jumlah;a++){
+            System.out.println(" ");
+            for(int b=0;b<=a;b++){
+                System.out.print("*"); 
+            }
+        }
+    }
+    
+}
